5140715a2777ccd52128e57b392e5f830e93a4f9
Lee <675516001@qq.com>
2026-06-12 13:29:21 +0800

    refactor: init 页面跳转路径适配 /console 前缀

--- frontend/src/pages/init/index.tsx
+++ frontend/src/pages/init/index.tsx
@@ -37,7 +37,7 @@ const Init: React.FC = () => {
       });
       message.success(t('init.initSuccess'));
       setTimeout(() => {
-        window.location.href = '/login';
+        window.location.href = '/console/login';
       }, 3000);
     } catch (error) {
       message.error(t('init.initFailed'));
